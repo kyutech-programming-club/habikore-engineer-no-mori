0c112cdccf3bbe54d06c40d3d91130d424802e61
kei1115 <keifukuda1115@gmail.com>
2022-02-05 15:15:05 +0900

    Add signinbtn in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import SigninBtn from '../components/SigninBtn';
 
 const Home: NextPage = () => {
-  return <h1>React & Next.js と AADB2C の連携</h1>;
+  return (
+    <>
+      <h1>React & Next.js と AADB2C の連携</h1>
+      <SigninBtn />
+    </>
+  );
 };
 
 export default Home;
