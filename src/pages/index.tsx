74426f78ad2319b5d7f490eee7c45b6c75dbc070
kei1115 <keifukuda1115@gmail.com>
2022-02-05 21:56:42 +0900

    Add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
 import Canvas from "../components/canvas";
+import SigninBtn from '../components/SigninBtn';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 const Home: NextPage = () => {
-  return <Canvas />;
+  const user = useCurrentUser();
+  return ( 
+  <>
+    <h1>React & Next.js と AADB2C の連携</h1>
+    <SigninBtn />
+    <p>こんにちは、{user?.familyName} {user?.givenName} さん</p>
+    <Canvas />
+  </>
+  );
 };
 
 export default Home;
