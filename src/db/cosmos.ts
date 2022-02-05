9d165828bab5cc9ea62792728d6f71e18364a998
kei1115 <keifukuda1115@gmail.com>
2022-02-06 07:36:11 +0900

    Add get_user function

--- src/db/cosmos.ts
+++ src/db/cosmos.ts
@@ -1,4 +1,7 @@
 import { CosmosClient } from "@azure/cosmos";
+import { userInfo } from "os";
+import { useRecoilState } from "recoil";
+import { userState } from "../atom/atoms";
 
 const config = {
   endpoint: "https://habimoridb.documents.azure.com:443/",
@@ -23,7 +26,7 @@ export async function create_user(b2c_id: string, name: string, state: number, c
   await container.items.create(newUser);
 }
 
-export async function update_user(b2c_id: string, name: string, state: number, commit: number, position: {x: number, y: number, url: string}[], id: string ): Promise<void> {
+export async function update_user(b2c_id: string, name: string, state: number, commit: number, position: {x: number, y: number, url: string}[] | null, id: string ): Promise<void> {
   const { endpoint, key, databaseId, containerId,} = config;
   const client = new CosmosClient({ endpoint, key });
   const database = client.database(databaseId);
@@ -37,4 +40,29 @@ export async function update_user(b2c_id: string, name: string, state: number, c
     id: id,
   };
   await container.item(id, b2c_id).replace(newUser);
+}
+
+export async function get_user(id: string ): Promise<void> {
+  const { endpoint, key, databaseId, containerId,} = config;
+  const client = new CosmosClient({ endpoint, key });
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+  const querySpec = {
+    query: "SELECT * from c"
+  };
+
+  const [user, setUser] = useRecoilState(userState)
+
+  const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
+  items.forEach((element) => {
+    if (element.id = id) {
+      user.id = element.id;
+      user.b2c_id = element.b2c_id;
+      user.name = element.name;
+      user.state = element.state;
+      user.totalPoint = element.totalPoint;
+      user.position = element.position;
+  }});
 }
