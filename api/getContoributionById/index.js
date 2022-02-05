8f06ebe0a272a0b3834140a53e8ef3aaf7a919a8
Kingoara <saku.itozono@icloud.com>
2022-02-05 23:12:36 +0900

    Calc total point

--- api/getContoributionById/index.js
+++ api/getContoributionById/index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async function (context, req) {
     const githubId = req.query.githubId;
-    contribution = await axios
+    allPoint = await axios
     .get('https://github-contributions-api.deno.dev/' + githubId + '.json')
     .then(response => {
         console.dir('response body:', response.data);
@@ -12,8 +12,21 @@ module.exports = async function (context, req) {
         console.log('err:', err);
     });
 
+    allPoint1d = allPoint.reduce( (newArr, elem) => {
+        return newArr.concat(elem)
+      }, [])
+
+    var date = new Date();
+    console.log(allPoint1d);
+
+    let point = 0
+    for(let i=364-date.getDate(); i < 364; i++){
+        point += allPoint1d[i].contributionCount;
+    }
+    console.log(point)
+
     context.res = {
         status: 200,
-        body: contribution,
+        body: point,
     };
 }
