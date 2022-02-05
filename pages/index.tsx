960b78dc269959534b2ae6622c0a7c8124f2325c
kei1115 <keifukuda1115@gmail.com>
2022-02-05 15:55:59 +0900

    Add indication of user's information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next';
 import SigninBtn from '../components/SigninBtn';
+import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 const Home: NextPage = () => {
+  const user = useCurrentUser();
   return (
     <>
       <h1>React & Next.js と AADB2C の連携</h1>
-      <SigninBtn />
+
+      <UnauthenticatedTemplate>
+        <SigninBtn />
+      </UnauthenticatedTemplate>
+
+      <AuthenticatedTemplate>
+        <p>
+          こんにちは、{user?.familyName} {user?.givenName} さん
+        </p>
+        <p>メールアドレス：{user?.email}</p>
+      </AuthenticatedTemplate>
     </>
   );
 };
