dbd3901e548605e09dafa3319b085cc387f0727e
syu-kwsk <wataru1165kwsk@gmail.com>
2022-02-06 15:16:32 +0900

    Add get payment by id

--- api/getPaymentById/index.js
+++ api/getPaymentById/index.js
@@ -1,108 +1,66 @@
 const mysql = require('mysql');
-
-// conn.connect(
-//     function (err) { 
-//         if (err) { 
-//             console.log("!!! Cannot connect !!! Error:");
-//             throw err;
-//         }
-//         else {
-//             console.log("Connection established.");
-//             readData();
-//         }
-//     }
-// );
-
-// function readData(){
-//     conn.query('SELECT * FROM inventory', 
-//         function (err, results, fields) {
-//             if (err) throw err;
-//             else console.log('Selected ' + results.length + ' row(s).');
-//             for (i = 0; i < results.length; i++) {
-//                 console.log('Row: ' + JSON.stringify(results[i]));
-//             }
-//             console.log('Done.');
-//         })
-//     conn.end(
-//         function (err) { 
-//             if (err) throw err;
-//             else  console.log('Closing connection.') 
-//     });
-// };
+const fs = require('fs');
+var payments = 0;
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
     let config = {
-        host: process.env["HOST"],
-        user: process.env["USER"],
-        password: process.env["PASSWORD"],
-        database: process.env["DATABASE_NAME"],
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
         port: 3306,
-        ssl: true
+        ssl: fs.readFileSync(process.env.ssl)
     };
 
     const conn = new mysql.createConnection(config);
 
-    function queryDatabase() {
-        conn.query('DROP TABLE IF EXISTS payments;', function (err, results, fields) { 
-            if (err) throw err; 
-            console.log('Dropped payment table if existed.');
-        })
-            conn.query('CREATE TABLE payments (id serial PRIMARY KEY, uid VARCHAR(50), date DATE, payment INTEGER);', 
-                function (err, results, fields) {
-                    if (err) throw err;
-            console.log('Created payments table.');
-        })
-        conn.query('INSERT INTO payments (uid, date, payment) VALUES (?, ?, ?);', ['91f19d68-7267-46e9-8f86-107619c6495e', '2022-02-01', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-                else console.log('Inserted ' + results.affectedRows + ' row(s).');
-            }
-        );
-
-        conn.query('INSERT INTO payments (uid, date, payment) VALUES (?, ?, ?);', ['91f19d68-7267-46e9-8f86-107619c6495e', '2022-02-02', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-                else console.log('Inserted ' + results.affectedRows + ' row(s).');
-            }
-        );
-
-        conn.query('INSERT INTO payments (uid, date, payment) VALUES (?, ?, ?);', ['91f19d68-7267-46e9-8f86-107619c6495e', '2022-02-03', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-                else console.log('Inserted ' + results.affectedRows + ' row(s).');
-            }
-        );
-
-        conn.query('INSERT INTO payments (uid, date, payment) VALUES (?, ?, ?);', ['91f19d68-7267-46e9-8f86-107619c6495e', '2022-02-04', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-                else console.log('Inserted ' + results.affectedRows + ' row(s).');
-            }
-        );
-
-        conn.query('INSERT INTO payments (uid, date, payment) VALUES (?, ?, ?);', ['91f19d68-7267-46e9-8f86-107619c6495e', '2022-02-05', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-                else console.log('Inserted ' + results.affectedRows + ' row(s).');
-            }
-        );
-        
-        conn.end(function (err) { 
-            if (err) throw err;
-            else  console.log('Done.') 
-        });
-    };
-
     conn.connect(
-        function (err) { 
-        if (err) { 
+        async function (err) {
+            console.log("connect")
+        if (err) {
             console.log("!!! Cannot connect !!! Error:");
             throw err;
         } else {
-        console.log("Connection established.");
-            queryDatabase();
+            console.log("Connection established.");
+            var dt = new Date();
+            dt.setDate(dt.getDate() - dt.getDate() + 1);
+            var y = dt.getFullYear();
+            var m = ('00' + (dt.getMonth()+1)).slice(-2);
+            var d = ('00' + dt.getDate()).slice(-2);
+            date = y + '-' + m + '-' + d;
+            // date = '2022-02-04 00:00:00';
+            console.log(date)
+
+            // queryDatabase();
+            let uid = req.query.id;
+            sql = `SELECT * FROM payments WHERE uid='${uid}' AND date >= '${date}'`
+            await conn.query(sql, function (err, results, fields) {
+                    if (err) throw err;
+                    else console.log('Selected ' + results.length + ' row(s).');
+
+                    for (i = 0; i < results.length; i++) {
+                        // console.dir(results[i].payment)
+                        payments += results[i].payment;
+                        console.log("payments: " + payments)
+                        console.log('Row: ' + JSON.stringify(results[i]));
+                    }
+                    console.log('Done.');
+                }
+            );
+            await conn.end(
+                function (err) { 
+                    if (err) throw err;
+                    else console.log('Closing connection.')
+                }
+            );
         }
     });
+    console.log(payments);
+    context.res = {
+        state: 200,
+        body: payments
+    }
+    payments = 0;
 }
