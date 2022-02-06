c79537a090ad579ddb10dc92e13b34f1c7bc1b98
kei1115 <keifukuda1115@gmail.com>
2022-02-06 13:12:44 +0900

    Add AccountInfo

--- src/db/cosmos.ts
+++ src/db/cosmos.ts
@@ -2,6 +2,26 @@ import { CosmosClient } from "@azure/cosmos";
 import { userInfo } from "os";
 import { useRecoilState } from "recoil";
 import { userState } from "../atom/atoms";
+import { AccountInfo } from '@azure/msal-browser';
+import { useMsal } from '@azure/msal-react';
+
+interface Account extends AccountInfo {
+  idTokenClaims: {
+    aud: string;
+    auth_time: number;
+    family_name: string;
+    given_name: string;
+    emails: string[];
+    iss: string;
+    nbf: number;
+    nonce: string;
+    sub: string;
+    tfp: string;
+    ver: string;
+    name: string;
+    newUser: boolean;
+  };
+}
 
 const config = {
   endpoint: "https://habimoridb.documents.azure.com:443/",
@@ -42,27 +62,38 @@ export async function update_user(b2c_id: string, name: string, state: number, c
   await container.item(id, b2c_id).replace(newUser);
 }
 
-export async function get_user(id: string ): Promise<void> {
+export async function get_user(): Promise<void> {
   const { endpoint, key, databaseId, containerId,} = config;
   const client = new CosmosClient({ endpoint, key });
   const database = client.database(databaseId);
   const container = database.container(containerId);
-  const querySpec = {
-    query: "SELECT * from c"
-  };
-
-  const [user, setUser] = useRecoilState(userState)
-
-  const { resources: items } = await container.items
-    .query(querySpec)
-    .fetchAll();
-  items.forEach((element) => {
-    if (element.id = id) {
-      user.id = element.id;
-      user.b2c_id = element.b2c_id;
-      user.name = element.name;
-      user.state = element.state;
-      user.totalPoint = element.totalPoint;
-      user.position = element.position;
-  }});
+  console.log("aa");
+  
+  const { accounts } = useMsal();
+  if (accounts.length > 0) {
+    const account = accounts[0] as Account;
+    const querySpec = {
+      query: "SELECT * from c"
+    };
+  
+    const [user, setUser] = useRecoilState(userState)
+  
+    const { resources: items } = await container.items
+      .query(querySpec)
+      .fetchAll();
+    items.forEach((element) => {
+      if (element.b2c_id == account.idTokenClaims?.sub) {
+        console.log(element.name);
+        setUser({
+          b2c_id: String(element.b2c_id),
+          name: String(element.name),
+          state: element.state,
+          totalPoint: element.totalPoint,
+          usedPoint: element.userdPoint,
+          position: element.position,
+          id: String(element.id),
+        })
+    }});
+  }
+  
 }
