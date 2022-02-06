2057d481f8517121e83ea04ca54bad9b220b0cbf
kei1115 <keifukuda1115@gmail.com>
2022-02-06 13:12:21 +0900

    Add get_user function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from "next";
 import Canvas from "../components/canvas";
 import Modal from "../components/Modal";
 import SigninBtn from "../components/SigninBtn";
+import { get_user } from "../db/cosmos";
 import useCurrentUser from "../hooks/useCurrentUser";
 
 const Home: NextPage = () => {
   const user = useCurrentUser();
+  get_user()
   return (
     <>
       <h1>React & Next.js と AADB2C の連携</h1>
       <SigninBtn />
+      <button onClick={async() => await get_user()}>取得</button>
       <p>
         こんにちは、{user?.familyName} {user?.givenName} さん
       </p>
