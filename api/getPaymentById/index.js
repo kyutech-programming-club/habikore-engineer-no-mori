e64d77a8ea9ce4a0ac3ef9773b38c87c85491eb7
syu-kwsk <wataru1165kwsk@gmail.com>
2022-02-06 09:18:49 +0900

    Connect azure func and cosmos in input

--- api/getPaymentById/index.js
+++ api/getPaymentById/index.js
@@ -1,13 +1,21 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, user) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const name = (req.query.name || (req.body && req.body.name));
+    console.log('id: ' + req.query.id);
+    if (!user)
+    {
+        context.log("User item not found");
+
+    }
+    else
+    {
+        context.log("Found User item, Description=" + user);
+        console.dir(user)
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: user
     };
 }
